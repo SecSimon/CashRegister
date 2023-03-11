4caf1afcade7854708cfa896a0485063521e9c99
Simon <s.liebl@protonmail.com>
2023-03-11 19:30:43 +0100

    Dialog adjustment for mobile version

--- src/app/pay-dialog/pay-dialog.component.ts
+++ src/app/pay-dialog/pay-dialog.component.ts
@@ -1,4 +1,4 @@
-import { Component, Inject, OnInit } from '@angular/core';
+import { Component, ElementRef, Inject, OnInit } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 export interface IPayDialogData {
@@ -14,9 +14,25 @@ export interface IPayDialogData {
 })
 export class PayDialogComponent implements OnInit {
 
-  constructor(public dialogRef: MatDialogRef<PayDialogComponent>, @Inject(MAT_DIALOG_DATA) public data: IPayDialogData) { }
+  constructor(public dialogRef: MatDialogRef<PayDialogComponent>, @Inject(MAT_DIALOG_DATA) public data: IPayDialogData, private el: ElementRef) { }
+
+  public colCount = 3;
+  public ranges = [
+    [5,10,15,20],
+    [25,30,35,40],
+    [45,50]
+  ];
 
   ngOnInit(): void {
+    const dialogWid = this.el.nativeElement.offsetWidth - 20;
+    if (dialogWid > 0) {
+      this.colCount = Math.floor(dialogWid/64) < 3 ? 3 : Math.floor(dialogWid/64);
+      const vals = this.ranges.reduce((acc, val) => acc.concat(val), []);
+      this.ranges = [];
+      for (let i = 0; i <  vals.length; i+=this.colCount) {
+        this.ranges.push(vals.slice(i, i+this.colCount));
+      }
+    } 
   }
 
   public CalcChange(): number {
