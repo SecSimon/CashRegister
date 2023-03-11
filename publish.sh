501268c54202ba7aa0f0f1434225ba3b5e5a6bf8
Simon <s.liebl@protonmail.com>
2023-03-11 17:06:21 +0100

    Change domain name

--- publish.sh
+++ publish.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-ng build --baseHref="https://www.simon-liebl.com/CashRegister/"
+ng build --baseHref="https://www.simon-liebl.de/CashRegister/"
 npx angular-cli-ghpages --dir=dist/CashRegister
 
